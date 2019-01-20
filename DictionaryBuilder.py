2571f390674bf7fe021966536adafec16f2fc852
Dana Cohen <Dana.Cohen1@hacktics.ey.net>
2019-01-20 21:44:05 +0200

    Finished building the dictionary

--- DictionaryBuilder.py
+++ DictionaryBuilder.py
@@ -4,16 +4,22 @@ import Lists
 
 class DictionaryBuilder:
 
-    def __init__(self, wordsList, numbersList):
+    def __init__(self, wordsList, numbersList, wordMaxLen = 1000, wordMinLen = 2):
         self.wordList = wordsList
         self.numbersList = numbersList
+        self.wordMaxLen = wordMaxLen
+        self.wordMinLen = wordMinLen
+
 
     def buildDictionary(self):
-        permotatedDict = sc.combinations([self.wordList, self.numbersList])
+        permotatedDict = sc.combinations([self.wordList, self.numbersList], self.wordMaxLen, self.wordMinLen)
         print("from DB: ", permotatedDict)
         return permotatedDict
 
 
 if __name__ == "__main__":
-    dictionary = DictionaryBuilder(Lists.words, Lists.numbers)
-    list = dictionary.buildDictionary()
+    dictionary = DictionaryBuilder(Lists.words, Lists.numbers, 1000, 6)
+    dictionary.buildDictionary()
+
+    with open("dictionary.txt", "r") as dict:
+        print(dict.read())
--- StringCombinations.py
+++ StringCombinations.py
@@ -1,20 +1,40 @@
+import Lists
 
-def combinations(p_list):
-    # connect cells in the list
-    concatList = concatenateCells(p_list)
-    return concatList
+def combinations(p_list, wordMaxLen, wordMinLen):
+    # Words before numbers:
+    itr1 = concatenateTwoListsCells(p_list, wordMaxLen, wordMinLen)
+    # itr1 before special characters:
+    itr11 = concatenateTwoListsCells([itr1, Lists.spec_characters], wordMaxLen, wordMinLen)
+    # special characters before itr1:
+    itr12 = concatenateTwoListsCells([Lists.spec_characters, itr1], wordMaxLen, wordMinLen)
+    # special characters before itr11:
+    itr21 = concatenateTwoListsCells([Lists.spec_characters, itr11], wordMaxLen, wordMinLen)
+    # special characters before itr12:
+    itr22 = concatenateTwoListsCells([Lists.spec_characters, itr12], wordMaxLen, wordMinLen)
+    # special characters after itr11:
+    itr211 = concatenateTwoListsCells([itr11, Lists.spec_characters], wordMaxLen, wordMinLen)
+    # special characters after itr12:
+    itr222 = concatenateTwoListsCells([itr12, Lists.spec_characters], wordMaxLen, wordMinLen)
+
+def getSpecialCamelCase(word):
+    word.lower()
 
 
-def concatenateCells(clist):
+
+def concatenateTwoListsCells(clist, wordMaxLen, wordMinLen):
+    if len(clist) > 2:
+        return
     rlist = []
-    index_before_list = 0
-    for clist_index in range(1, len(clist)):
-        # 2 lists from clist list.
-        curr_list = clist[clist_index]
-        before_list = clist[clist_index-1]
-
-        # concatenate each word from before list to current list.
-        for index_before_list in range(len(before_list)):
-            for index in range(len(curr_list)):
-                rlist.append(before_list[index_before_list] + curr_list[index])
+    with open("dictionary.txt", "a") as dict:
+        for clist_index in range(1, len(clist)):
+            # 2 lists from clist list.
+            curr_list = clist[clist_index]
+            before_list = clist[clist_index-1]
+            # concatenate each word from before list to current list.
+            for index_before_list in range(len(before_list)):
+                for index in range(len(curr_list)):
+                    newWord = before_list[index_before_list] + curr_list[index]
+                    if wordMinLen <= len(newWord) <= wordMaxLen:
+                        dict.write(newWord+"\n")
+                        rlist.append(newWord)
     return rlist
